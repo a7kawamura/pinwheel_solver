11c5ac17c3d86baaeeb27b1b0a1e0e0b13b81b19
Akitoshi Kawamura <kawamura@kurims.kyoto-u.ac.jp>
2026-06-19 11:36:29 +0900

    find_cycleの結果をタグ型に

--- include/pinwheel/solver.hpp
+++ include/pinwheel/solver.hpp
@@ -10,6 +10,10 @@
 
 namespace pinwheel {
 
+struct Unschedulable {};
+struct Interrupted {};
+using CycleResult = std::variant<Schedule, Unschedulable, Interrupted>;
+
 struct SolveResult {
   PinwheelInstance instance;
   Schedule schedule;
@@ -17,9 +21,9 @@ struct SolveResult {
 
 // find_cycle_sub(v, dead, visited, path, done): 
 // 実行開始時における約束として、dead に属するどの状態からも閉路に到達不能であり、visited は初期状態から v への経路（但し初期状態から i 番目の状態を i へ写す写像として表す）、path はそれに対応する（有限の）日割である。
-// v から閉路に到達可能であるとき、その閉路を返す。さもなくば、v から到達可能な状態をすべて dead に加えた上で、真を返す。但し計算中に done が真になったことに気づくと中断して偽を返す。
+// v から閉路に到達可能であるとき、その閉路を返す。さもなくば、v から到達可能な状態をすべて dead に加えた上で、Unschedulable() を返す。但し計算中に done が真になったことに気づくと中断して Interrupted() を返す。
 template <typename Policy>
-std::variant<Schedule, bool> find_cycle_sub (
+CycleResult find_cycle_sub (
     const State& v, 
     std::unordered_set<State>& dead, 
     std::unordered_map<State, size_t>& visited, 
@@ -40,22 +44,22 @@ std::variant<Schedule, bool> find_cycle_sub (
     
     State w = ww.value();
     if (dead.contains(w)) continue;
-    if (done) return false;
+    if (done) return Interrupted();
     
     path.periods.push_back(v.q[j].second);
-    std::variant<Schedule, bool> r = find_cycle_sub<Policy>(w, dead, visited, path, done);
+    CycleResult r = find_cycle_sub<Policy>(w, dead, visited, path, done);
     path.periods.pop_back();
     
-    if (std::holds_alternative<Schedule>(r) or not std::get<bool>(r)) return r; // w から到達可能な閉路が見つかった場合はその閉路を（v から到達可能な閉路として）返すし、計算中断ならば中断と返す
+    if (std::holds_alternative<Schedule>(r) or std::holds_alternative<Interrupted>(r)) return r; // w から到達可能な閉路が見つかった場合はその閉路を（v から到達可能な閉路として）返すし、計算中断ならば中断と返す
   }
   dead.insert(v);
   visited.erase(v);
-  return true;
+  return Unschedulable();
 }
 
-// find_cycle(c, done): 状態グラフ上の閉路検出の方法により、周期列 c の割当可能性を判定する。割当可能なら日割の一つを返し、不能なら真を返す。計算中に done が真になると中断して偽を返す。
+// find_cycle(c, done): 状態グラフ上の閉路検出の方法により、周期列 c の割当可能性を判定する。割当可能なら日割の一つを返し、不能なら Unschedulable() を返す。計算中に done が真になると中断して Interrupted() を返す。
 template <typename Policy>
-std::variant<Schedule, bool> find_cycle (const PinwheelInstance& c, std::atomic<bool>& done) {
+CycleResult find_cycle (const PinwheelInstance& c, std::atomic<bool>& done) {
   std::unordered_set<State> dead{};
   std::unordered_map<State, size_t> visited{};
   State initial_state = Policy::create_initial_state(c);
@@ -64,6 +68,7 @@ std::variant<Schedule, bool> find_cycle (const PinwheelInstance& c, std::atomic<
 }
 
 // solve_instances(cs): cs の各周期列の割当可能性を並列に調べる。割当可能なものが一つでも見つかったらその周期列と日割の組を返す。見つからなければstd::nulloptを返す。
+// #pragma omp parallel for を使用し、利用可能な最大スレッド数を活かしながら、各スレッドへ動的に周期列の探索タスクを割り振る。
 template <typename Policy>
 std::optional<SolveResult> solve_instances (const std::vector<PinwheelInstance>& cs) {
   std::atomic<bool> done{false};
@@ -96,13 +101,11 @@ std::vector<PinwheelInstance> all_folds (const PinwheelInstance& c) {
   return r;
 }
 
-// find_and_cache(c, known_schedules): 写像 known_schedules に書かれているのは、既知の周期列と正しい日割の組であるとする。このとき、周期列 c は割当可能か調べ、真偽を返す。これを all_folds(c) の各周期列の割当可能性を並列に調べることで行う。まず known_schedules から直ちに判るか調べる。判らなければ、並列に find_cycle で調べる。割当可能なら、割当できた周期列とその日割とを表示し、known_schedules に記入する。割当不能なら UNSCHEDULABLE と表示する。
-// #pragma omp parallel for を使用し、利用可能な最大スレッド数を活かしながら、各スレッドへ動的に周期列の探索タスクを割り振る。
+// find_and_cache(c, known_schedules): 写像 known_schedules に書かれているのは、既知の周期列と正しい日割の組であるとする。このとき、周期列 c は割当可能か調べ、真偽を返す。これを all_folds(c) の各周期列の割当可能性を並列に調べることで行う。まず known_schedules から直ちに判るか調べる。判らなければ、solve_instances を呼んで調べる。割当可能なら、割当できた周期列とその日割とを表示し、known_schedules に記入する。割当不能なら UNSCHEDULABLE と表示する。
 template <typename Policy>
 bool find_and_cache (const PinwheelInstance& c, std::unordered_map<PinwheelInstance, Schedule>& known_schedules) {
   std::vector<PinwheelInstance> cs = all_folds<Policy>(c);
   for (const auto& d : cs) if (known_schedules.contains(d)) return true;
-
   // 【進捗ログ】現在どの周期列を検証しているかリアルタイムで表示
   std::cout << "Checking: " << c.to_string() << " (folds: " << cs.size() << ")" << std::endl;
 
